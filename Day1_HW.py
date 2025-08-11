deae8a28b3ce6045125a4105594becf5c8616697
sushhh05 <sushantnigadi05@gmail.com>
2025-08-11 18:44:11 +0530

    Homework

--- Day1_HW.py
+++ Day1_HW.py
@@ -1,1 +1,32 @@
-print("Hello World!")
+# 1. Write a program that takes the radius as input and calculates the area and circumference of a circle.
+
+def circle():
+    rad = int(input("Enter the radius of circle : "))
+    area = 3.14 * (rad ** 2)
+    circumference = 2 * 3.14 * rad
+    print(f"Area of circle is : {round(area, 2)}")
+    print(f"Circumference of circle is : {round(circumference, 2)}")
+
+circle()
+
+
+# 2. Write a program to check if a number is divisible by both 3 and 5.
+
+n = int(input("Enter a number: "))
+
+if n % 3 == 0 and n % 5 == 0:
+    print("The number is divisible by both 3 and 5.")
+else:
+    print("The number is not divisible by both 3 and 5.")
+
+
+# 3. Write a program that takes a 4-digit number from the user and prints the sum of its digits.
+
+n = int(input("Enter a 4-digit number: "))
+
+sum = 0
+for i in range (4):
+    sum += n % 10
+    n = n // 10
+
+print(sum)
--- Day2_HW.py
+++ Day2_HW.py
@@ -1,68 +1,25 @@
-#Q2. PASSWORD STRENGTH VALIDATOR
-password = input("Enter your password: ")
+# 1. Write a program to print the factorial of a number using a for loop.
 
-upper = any(c.isupper() for c in password)
-lower = any(c.islower() for c in password)
+n = int(input("Enter a number: "))
+def factorial(n):
+    if (n == 0 or n == 1):
+        return 1
+    return n * factorial(n-1)
 
-length = len(password)
+print(f"The factorial of {n} is {factorial(n)}")
 
-if length > 8 and upper and lower :
-    print("Strong Password")
-elif length == 8:
-    print("Moderate Password")
+
+# 2. Write a program that prints all numbers from 1 to 100 that are divisible by 7 but not a multiple of 5.
+
+for i in range (1, 101):
+    if i % 7 == 0 and i % 5 != 0:
+        print(i)
+
+# 3. Write a program that takes a number and prints whether it is a palindrome or not.
+
+n = (input("Enter a number: "))
+
+if n == n[::-1] :
+    print("The number is a palindrome")
 else:
-    print("Weak Password")
-
-#Q3.NESTED GRADE EVALUATION WITH ATTENDANCE
-def exam_Result(marks, attendance):
-        if marks >= 50 and attendance >= 75:
-            print("pass")
-        elif marks < 50 and attendance < 75:
-            print("fail due to both")
-        elif marks < 50:
-            print("fail due to marks")
-        elif attendance < 75:
-            print("fail due to attendance")
-exam_Result(0,80)
-
-#Q4. TRAFFIC FINE CALCULATOR
-def fine_calculator(car_spd, truck_spd, bike_spd):
-    if car_spd > 60:
-        if car_spd <= 70:
-            print("500 fine") 
-        elif car_spd < 80:
-            print("1000 fine")
-        else:
-            print("2000 fine + license ban")
-    if truck_spd > 60:
-        if truck_spd <= 70:
-            print("1000 fine") 
-        elif truck_spd < 75:
-            print("2000 fine")
-        else:
-            print("4000 fine + license ban")
-    if bike_spd > 60:
-        if bike_spd <= 70:
-            print("250 fine") 
-        elif bike_spd < 85:
-            print("500 fine")
-        else:
-            print("1000 fine + license ban")
-
-fine_calculator(70,65,65)
-
-#Q5. MULTI LOAN ELIGIBILTY CHECKER
-age = int(input("enter your age: "))
-income = int(input("enter your income: "))
-credit_score = int(input("enter your credit_score: "))
-
-if (age >= 21 and age <= 60) and income >= 25000 and (credit_score >= 600 and credit_score <= 850):
-    print("approve loan")
-elif (age < 21 or age > 60) and income < 25000 and (credit_score < 600 or credit_score > 850):
-    print("rejected due to multiple reasons")
-elif (age < 21 or age > 60):
-    print("rejected due to age")
-elif income < 25000:
-    print("rejected due to low income")
-elif credit_score < 600 or credit_score > 850:
-    print("rejected due to poor credit")  
+    print("The number is not a palindrome")
